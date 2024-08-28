788665bc41172ea2f1741a23a6ca667d22f0bf92
Aman Kumar <137717130+Aman07kumar@users.noreply.github.com>
2024-08-29 04:58:31 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,3 @@
-// API Endpoints
-
 export const USER_API_END_POINT = "https://jobportal-backend-p8da.onrender.com/api/v1/user";
 export const JOB_API_END_POINT = "https://jobportal-backend-p8da.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT = "https://jobportal-backend-p8da.onrender.com/api/v1/application";
