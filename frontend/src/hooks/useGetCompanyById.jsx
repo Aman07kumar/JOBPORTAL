563ffe68bae309ed60b8253abd11e0e2e6e11118
Aman Kumar <137717130+Aman07kumar@users.noreply.github.com>
2024-08-29 05:08:01 +0530

    Update useGetCompanyById.jsx

--- frontend/src/hooks/useGetCompanyById.jsx
+++ frontend/src/hooks/useGetCompanyById.jsx
@@ -1,26 +1,27 @@
-import { setSingleCompany } from '@/redux/companySlice'
-import { setAllJobs } from '@/redux/jobSlice'
-import { COMPANY_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant'
-import axios from 'axios'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { setSingleCompany } from '@/redux/companySlice';
+import { COMPANY_API_END_POINT } from '@/utils/constant';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 const useGetCompanyById = (companyId) => {
     const dispatch = useDispatch();
-    useEffect(()=>{
+    useEffect(() => {
         const fetchSingleCompany = async () => {
             try {
-                const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`,{withCredentials:true});
-                console.log(res.data.company);
-                if(res.data.success){
+                const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`, { withCredentials: true });
+                console.log('Company Response:', res.data);
+                if (res.data.success) {
                     dispatch(setSingleCompany(res.data.company));
+                } else {
+                    console.warn('Fetch Company Failed:', res.data.message);
                 }
             } catch (error) {
-                console.log(error);
+                console.error('Error fetching company:', error);
             }
-        }
+        };
         fetchSingleCompany();
-    },[companyId, dispatch])
-}
+    }, [companyId, dispatch]);
+};
 
-export default useGetCompanyById
+export default useGetCompanyById;
