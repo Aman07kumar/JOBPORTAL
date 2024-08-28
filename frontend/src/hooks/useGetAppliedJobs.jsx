d71a9294727657c46ad3d71a5cb510019944b1ad
Aman Kumar <137717130+Aman07kumar@users.noreply.github.com>
2024-08-29 05:07:37 +0530

    Update useGetAppliedJobs.jsx

--- frontend/src/hooks/useGetAppliedJobs.jsx
+++ frontend/src/hooks/useGetAppliedJobs.jsx
@@ -1,25 +1,28 @@
-import { setAllAppliedJobs } from "@/redux/jobSlice";
-import { APPLICATION_API_END_POINT } from "@/utils/constant";
-import axios from "axios"
-import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { setAllAppliedJobs } from '@/redux/jobSlice';
+import { APPLICATION_API_END_POINT } from '@/utils/constant';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 const useGetAppliedJobs = () => {
     const dispatch = useDispatch();
 
-    useEffect(()=>{
+    useEffect(() => {
         const fetchAppliedJobs = async () => {
             try {
-                const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {withCredentials:true});
-                console.log(res.data);
-                if(res.data.success){
+                const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, { withCredentials: true });
+                console.log('Applied Jobs Response:', res.data);
+                if (res.data.success) {
                     dispatch(setAllAppliedJobs(res.data.application));
+                } else {
+                    console.warn('Fetch Applied Jobs Failed:', res.data.message);
                 }
             } catch (error) {
-                console.log(error);
+                console.error('Error fetching applied jobs:', error);
             }
-        }
+        };
         fetchAppliedJobs();
-    },[])
+    }, [dispatch]);
 };
+
 export default useGetAppliedJobs;
