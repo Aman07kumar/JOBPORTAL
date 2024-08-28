deccea8f6d6152532b504cd6220ed609c7e10a37
Aman Kumar <137717130+Aman07kumar@users.noreply.github.com>
2024-08-29 04:53:15 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,6 @@
-export const USER_API_END_POINT="https://jobportal-backend-p8da.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://jobportal-backend-p8da.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://jobportal-backend-p8da.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://jobportal-backend-p8da.onrender.com/v1/company";
-// export const URL = "https://jobportal-veiu.onrender.com/"
+// API Endpoints
+
+export const USER_API_END_POINT = "https://jobportal-backend-p8da.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://jobportal-backend-p8da.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://jobportal-backend-p8da.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://jobportal-backend-p8da.onrender.com/api/v1/company";
